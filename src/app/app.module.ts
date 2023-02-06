e040df6b3cfdf67e82e4e8768b6f04e04fdc1d09
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-02-06 18:40:59 -0300

    component NAV criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,9 +24,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+import { NavComponent } from './components/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
