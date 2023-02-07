fabdf25fe498fe2b6cee90c8c20144343198ebaa
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-02-06 22:28:39 -0300

    rota para component LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { 
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
